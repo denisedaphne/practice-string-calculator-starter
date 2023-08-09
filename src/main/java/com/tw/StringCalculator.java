2999f1fceb4b973e675d60208154d80f84a694bd
Daphne Balane <balanedaphne20@gmail.com>
2023-08-09 13:57:49 +0800

    (Java-exam): Added source code to StringCalculator.java

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,7 +1,32 @@
 package com.tw;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
-    public int add(String string) {
-        return 0;
+    public int add(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+
+        String delimiter = ",|\n";
+        String numbers = input;
+
+        if (input.startsWith("//")) {
+            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+            if (matcher.find()) {
+                delimiter = Pattern.quote(matcher.group(1));
+                numbers = matcher.group(2);
+            }
+        }
+
+        String[] numberArray = numbers.split(delimiter);
+        int sum = 0;
+
+        for (String number : numberArray) {
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
     }
 }
